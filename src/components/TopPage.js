35004c357f1e2a0e81a5be1925c9e78c78b82f19
Jaoudat <jaoudat.joudinho@hotmail.fr>
2023-03-19 11:07:17 +0100

    V18 Main : - Ajout MouseFlow

--- src/components/TopPage.js
+++ src/components/TopPage.js
@@ -47,6 +47,15 @@ function TopPage() {
         }
     }
 
+
+    window._mfq = window._mfq || [];
+    (function() {
+        var mf = document.createElement("script");
+        mf.type = "text/javascript"; mf.defer = true;
+        mf.src = "//cdn.mouseflow.com/projects/7f88e616-22dd-4beb-9643-495e7fae9246.js";
+        document.getElementsByTagName("head")[0].appendChild(mf);
+    })();
+
     function returnHome(){
         if (router.pathname === "/")
             router.reload(window.location.pathname);
