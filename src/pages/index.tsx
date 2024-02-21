ae4958042faee3d13929470f6671de6c40e20fd5
SABALBALJaoudat <72742901+SABALBALJaoudat@users.noreply.github.com>
2024-02-21 16:42:23 +0000

    fix code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Head from 'next/head'
-import HomePage from './Home';
+import Acceuil from './Home';
 
 export default function Home() {
     return (
@@ -14,7 +14,7 @@ export default function Home() {
 
             </Head>
 
-            <HomePage />
+            <Acceuil />
 
 
         </>
