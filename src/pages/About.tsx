9216e6c2909905534819ccd55e6d8a71d2bbbb0e
SABALBALJaoudat <72742901+SABALBALJaoudat@users.noreply.github.com>
2024-03-27 15:44:08 +0000

    Ajout premiere version Svg Curve Animation

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,6 @@
+'use client'
 import React from 'react'
-import IconButton from "@/components/IconButton";
+import { useRef, useEffect } from 'react';
 import { BsDownload } from "react-icons/bs";
 import Image from "next/image";
 import photo from "@/image/Photo de profil.png";
@@ -10,7 +11,102 @@ import '../styles/About.module.scss'
 import { motion } from 'framer-motion';
 import styles from '../styles/About.module.scss'
 
+interface MouseEvent {
+    movementY: number;
+    clientX: number;
+  }
+
 function About() {
+// Define a reference to an SVGPathElement
+const path = useRef<SVGPathElement>(null);
+
+// Initialize progress, x, time, and reqId variables
+let progress = 0;
+let x = 0.5;
+let time = Math.PI / 2;
+let reqId: number | null = null;
+
+// Use the useEffect hook to set the path on component mount
+useEffect(() => {
+  setPath(progress);
+}, []);
+
+// Define a function to set the path of the SVG element
+const setPath = (progress: number) => {
+  // Get the width of the window
+  const width = window.innerWidth * 1;
+
+  // Set the "d" attribute of the SVG path element using a quadratic Bézier curve
+  path.current?.setAttributeNS(
+    null,
+    "d",
+    `M0 250 Q${width * x} ${250 + progress}, ${width} 250`
+  );
+};
+
+// Define a linear interpolation function
+const lerp = (x: number, y: number, a: number) => x * (1 - a) + y * a;
+
+// Define a function to handle mouse enter events
+const manageMouseEnter = () => {
+  // If there is an animation frame request, cancel it and reset the animation
+  if (reqId) {
+    cancelAnimationFrame(reqId);
+    resetAnimation();
+  }
+};
+
+// Define a function to handle mouse move events
+const manageMouseMove = (e: MouseEvent) => {
+  // Get the movementY and clientX properties from the event object
+  const { movementY, clientX } = e;
+
+  // Get the bounding rectangle of the SVG path element
+  const pathBound = path.current?.getBoundingClientRect();
+
+  // If the bounding rectangle exists, update x and progress and set the path
+  if (pathBound) {
+    x = (clientX - pathBound.left) / pathBound.width;
+    progress += movementY;
+    setPath(progress);
+  }
+};
+
+// Define a function to handle mouse leave events
+const manageMouseLeave = () => {
+  // Start animating out
+  animateOut();
+};
+
+// Define a function to animate out
+const animateOut = () => {
+  // Calculate newProgress using sine of time
+  const newProgress = progress * Math.sin(time);
+
+  // Update progress using linear interpolation towards zero
+  progress = lerp(progress, 0, 0.025);
+
+  // Increment time by 0.2
+  time += 0.2;
+
+  // Set the path using newProgress
+  setPath(newProgress);
+
+  // If progress is greater than a threshold, request another animation frame,
+  // otherwise reset the animation.
+  if (Math.abs(progress) > 0.75) {
+    reqId = requestAnimationFrame(animateOut);
+  } else {
+    resetAnimation();
+  }
+};
+
+// Define a function to reset the animation variables
+const resetAnimation = () => {
+  time = Math.PI / 2;
+  progress = 0;
+};
+
     return (
         <div>
             <div className="min-h-screen flex flex-col overflow-hidden bg-neutral-50">
@@ -36,11 +132,42 @@ function About() {
                                             </p> */}
                                             <p className={"text-justify text-base leading-7"}>
                                                 Salut !
-                                                Je suis Jaoudat, un développeur Front-End et Ux/Ui Designer passionné et motivé. 
+                                                Je suis Jaoudat, un développeur Front-End et Ux/Ui Designer passionné et motivé.
+                                                Je te souhaite une bienvenue chaleureuse sur mon site web.
+                                                Je vais t'expliquer brievement ce que tu peux trouver dessus.
+                                                Ce site web est un portfolio qui regroupe mes projets de développement et de design.
+                                                MAIS PAS QUE !
+                                                Tu vas aussi y retrouver beaucoup d'element caché ici et la, car ce site est aussi un peu
+                                                un terrain de jeu pour moi. Cest une sorte de bac à sable pour tester des nouvelles choses.
+                                                Ce sont surtout des elements graphiques et des animations.
+                                                A termes, je souhaite que ce site serve également de Hub pour mes projets Webs professionnels
+                                                (lorsque ceux-ci sont disponible en fonction des clients) mais aussi personnels.
+                                                J'espère que tu vas apprécier ta visite, et que tu auras le temps de repérer toutes les 
+                                                subtilités 
                                             </p>
 <svg viewBox="0 0 230 126" fill="none" preserveAspectRatio='xMidYMax meet'>
 <path className={styles.path} strokeWidth="3" d="M1.5 48.0006C10.3333 32.8339 34.7 2.40059 61.5 2.00059C88.3 1.60059 105.5 28 118 45.0006C130.5 62.0012 216.94 98.0712 195 74.5006C186.385 65.2455 190.636 56.7575 215.5 45.0006C273.038 17.7942 107.302 19.9522 99.5 64.5C92.1645 106.383 149.5 109 140.5 107C131.5 105 113.3 87.8 130.5 95C152 104 157.5 106 157.5 109C157.5 112 146.5 116 130.5 122C117.7 126.8 125.167 121 130.5 117.5L140.5 111" stroke="black" stroke-width="3" stroke-linecap="round"/>
 </svg>
+{/* <div className="relative w-full h-px mb-5">
+        <div
+          onMouseEnter={() => {
+            manageMouseEnter();
+          }}
+          onMouseMove={(e) => {
+            manageMouseMove(e);
+          }}
+          onMouseLeave={() => {
+            manageMouseLeave();
+          }}
+          className="relative z-10 h-10 w-full top-[-40px]"
+        ></div>
+        <svg className="absolute w-full h-[500px] top-[-250px]">
+          <path
+            ref={path}
+            className="stroke-current text-black stroke-[1px] fill-none"
+          ></path>
+        </svg>
+      </div> */}
                                             <div className={"text-justify text-base leading-7 mt-5 flex flex-wrap items-center"}>
                                                 You can have a look to my CV with all my projects :
                                                 <a href='/documents/Cv%20Jaoudat%20SABALBAL%20Designer%20Ux%20Ui%20Dev%20Front.pdf'
@@ -74,6 +201,26 @@ function About() {
                                         animate={{ y: 0, opacity: 1 }}
                                         exit={{ y: "-100px", opacity: 0 }}
                                         transition={{ duration: 1 }}>
+                                            <div className="relative w-80 h-5 mb-10 mt-10">
+        <div
+          onMouseEnter={() => {
+            manageMouseEnter();
+          }}
+          onMouseMove={(e) => {
+            manageMouseMove(e);
+          }}
+          onMouseLeave={() => {
+            manageMouseLeave();
+          }}
+          className="relative z-10 h-10 w-full top-[-40px]"
+        ></div>
+        <svg className="absolute w-full h-[500px] top-[-250px]">
+          <path
+            ref={path}
+            className="stroke-current text-black stroke-[1px] fill-none"
+          ></path>
+        </svg>
+      </div>
                                         <Skill />
                                     </motion.div>
                                 </div>
